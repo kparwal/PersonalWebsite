57e03b9c2fc02e7af9ab949ba7b1ee41032bcb40
kparwal <keshav.parwal@decisionfirst.com>
2015-07-22 14:23:51 -0400

    updated about command

--- js/interactive.js
+++ js/interactive.js
@@ -22,15 +22,10 @@ function returncommand(term, command) {
     return '\nHi, [[;lime;]' + firstName + ']! Enter the command "help" to see your options.\n';
   } else {
     if (command == 'help') {
-      return "\nThese are the currently supported commands\n\nabout\nproject\nclear\n";
+      return "\nThese are the currently supported commands\n\nabout\nproject\nskill\nclear\n";
     }
-    if (command == 'about') {
-      return '\n[[;lime;]My name is Keshav Parwal and I am a budding developer and software engineer studying at the Georgia Institute of Technology.' +
-      ' My interests include Web Development, Android Development, and Information Security and Cryptography.\n\nThere is nothing more enjoyable than making'+
-      ' what people considered impossible yesterday into today\'s possibility. As such, I like to live on the cutting edge of science, technology, and' +
-      ' mathematics. I am a natural problem solver, but more importantly I am an effective and creative communicator. No one person can solve any worthwhile' +
-      ' problem on their own, so it is important to be able to bring in the right talent and place it in the right environment. In other words, using the right' +
-      ' tool for the right job.]\n';
+    if (command.trim().split(' ')[0] == 'about') {
+      return getAbout(command.trim().split(' ')[1]);
     }
     if (command.trim().split(' ')[0] == 'project') {
       var project = command.trim().split(' ')[1];
@@ -58,6 +53,9 @@ function returncommand(term, command) {
       }
       return '\nUSAGE: project [project-name]\nAvailable Projects:\n\tShopping With Friends\n\tSalesforce BusinessObjects Integration\n\tMathShell\n\tDPU-Simulator\n\tKeshavShell (this)\n\tSmartLibrary';
     }
+    if (command.trim().split(' ')[0] == 'skill') {
+      return getSkill(command.trim().split(' ')[1]);
+    }
     return "[[;red;]\nSorry, that command wasn't recognized. Enter 'help' to see a list of valid commands]\n";
   }
 }
@@ -68,4 +66,48 @@ $(function() {
 
 function displayTerminal() {
   $('#terminal').slideToggle("slow");
+}
+
+function getSkill(skill) {
+  if (skill) {
+    switch (skill) {
+      case "Java":
+        return "\n[[;#CC0052;]Java is my most comfortable programming language. Beyond just the language, I have experience with JavaFX, Tomcat, Glassfish, Java EE, and Eclipse. I have written various servlet programs for use in Tomcat, as well as programs that leverage the cryptographic functions in Java.]\n"
+        break;
+      case "Python":
+        return "\n[[;#CC0052;]Although I am newer to Python, I've worked with Python-based web frameworks like Flask as well as Jython.]\n"
+        break;
+      case "Web":
+        return "\n[[;#CC0052;]A lot of my experience is in web technologies. I am highly adept with HTML, CSS, and JavaScript, but I also have worked with Node.js, AngularJS, and jQuery. I have also used server-side technologies like Tomcat.]\n"
+        break;
+      case "C":
+        return "\n[[;#CC0052;]I am a proficient C programmer, with a little experience in C++. I am also familiar with x86 assembly.]\n"
+        break;
+      case "Android":
+        return "\n[[;#CC0052;]I have extensive experience with Android, including the Google Maps API and Parse cloud storage. Many of my side projects are Android apps.]\n"
+        break;
+      case "Other":
+        return "\n[[;#CC0052;]I am highly familiar with Linux, web servers, cryptography, advanced data structures and algorithms, and digital chip design. I speak English, Hindi, and Spanish, and I am learning Japanese.]\n"
+        break;
+    }
+  }
+  return '\nUSAGE: skill [project-name]\nAvailable Skills:\n\tJava\n\tPython\n\tWeb\n\tC\n\tAndroid\n\tOther\n';
+}
+
+function getAbout(mod) {
+  switch (mod) {
+    case "professional":
+      return '\n[[;lime;]My name is Keshav Parwal and I am a budding developer and software engineer studying at the Georgia Institute of Technology.' +
+      ' My interests include Web Development, Android Development, and Information Security and Cryptography.\n\nThere is nothing more enjoyable than making'+
+      ' what people considered impossible yesterday into today\'s possibility. As such, I like to live on the cutting edge of science, technology, and' +
+      ' mathematics. I am a natural problem solver, but more importantly I am an effective and creative communicator. No one person can solve any worthwhile' +
+      ' problem on their own, so it is important to be able to bring in the right talent and place it in the right environment. In other words, using the right' +
+      ' tool for the right job.]\n';
+      break;
+    case "personal":
+      return '\n[[;#009900;]Beyond the realm of computer science and professional skills, my hobbies include bicycling, writing, and digital design. I am an avid gamer and photographer.\n\nI love to play Quiz Bowl, and I am a huge history buff. I love learning about ancient cultures and languages. In that vein, I also love astronomy and physics (I am signed up for the NASA newsletter) and everything in between.]';
+      break;
+    default:
+      return '\nUSAGE: about [flag]\nFlags:\n\tprofessional\n\tpersonal';
+  }
 }
